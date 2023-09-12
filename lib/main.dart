861f253a317be7aa0d768817be7179961c8fa95d
이정빈 <ralph.lee@live.ca>
2023-09-12 09:30:02 +0900

    Add: Toggling camera between front and rear

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,175 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+Future<void> main() async {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+  // can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 기기에서 사용 가능한 카메라 목록 불러오기
+  final cameras = await availableCameras();
+
+  // 사용 가능한 카메라 중 첫 번째 카메라 사용
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(
+        // Pass the available cameras list to the TakePictureScreen widget.
+        availableCameras: cameras,
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// Camera Page
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    super.key,
+    required this.availableCameras,
+  });
 
-  // This widget is the root of your application.
+  final List<CameraDescription> availableCameras;
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+  int selectedCameraIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Camera controller to display the current output from the camera.
+    _controller = CameraController(
+      widget.availableCameras[selectedCameraIndex], // Get a specific camera from the list of available cameras.
+      ResolutionPreset.medium, // Define the resolution to use.
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // void _toggleCameraLens() {
+  //   // get current lens direction (front / rear)
+  //   final lensDirection =  _controller.description.lensDirection;
+  //   CameraDescription newDescription;
+  //   if(lensDirection == CameraLensDirection.front){
+  //     newDescription = _availableCameras.firstWhere((description) => description.lensDirection == CameraLensDirection.back);
+  //   }
+  //   else{
+  //     newDescription = _availableCameras.firstWhere((description) => description.lensDirection == CameraLensDirection.front);
+  //   }
+  //
+  //   if(newDescription != null){
+  //     _initCamera(newDescription);
+  //   }
+  //   else{
+  //     print('Asked camera not available');
+  //   }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Take a picture')),
+      // You must wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner until the
+      // controller has finished initializing.
+      body: Column(
+        children: [
+          FutureBuilder<void>(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                // If the Future is complete, display the preview.
+                return CameraPreview(_controller);
+              } else {
+                // Otherwise, display a loading indicator.
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              selectedCameraIndex == 0 ? selectedCameraIndex = 1 : selectedCameraIndex = 0;
+
+              setState(() {
+                _controller = CameraController(
+                  widget.availableCameras[selectedCameraIndex], // Get a specific camera from the list of available cameras.
+                  ResolutionPreset.medium, // Define the resolution to use.
+                );
+              });
+
+              print("--------");
+              print(selectedCameraIndex);
+              // Next, initialize the controller. This returns a Future.
+              _initializeControllerFuture = _controller.initialize();
+            },
+            child: Icon(Icons.camera_enhance_outlined),
+          ),
+          Text(selectedCameraIndex == 0 ? "Front camera" : "Rear camera"),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // 버튼 누를 시 카메라 화면의 캡쳐본을 보여주는 화면으로 이동
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // Take the Picture in a try/catch block.
+          try {
+            print('-----------------success1------------------');
+
+            await _initializeControllerFuture;
+            print('-----------------success2------------------');
+
+            // Attempt to take a picture and get the file `image` where it was saved.
+            final image = await _controller.takePicture();
+            print('-----------------success3------------------');
+
+            if (!mounted) return;
+
+            // Display picture if it was taken.
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  // Pass the automatically generated path to the DisplayPictureScreen widget.
+                  imagePath: image.path,
+                ),
+              ),
+            );
+          } catch (e) {
+            print('-----------------error------------------');
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// --------------------------------
+// 찍은 사진 보여주는 위젯
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const DisplayPictureScreen({super.key, required this.imagePath});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(title: const Text('Picture Taken')),
+      body: Image.file(File(imagePath)),
     );
   }
 }
