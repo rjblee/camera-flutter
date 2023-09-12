6f4cc81f76aad8c0e411dab8eda595688944169f
이정빈 <ralph.lee@live.ca>
2023-09-12 17:00:49 +0900

    Add: signUp and login functions

---
+++ lib/authentication.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Authentication extends StatefulWidget {
+  const Authentication({super.key});
+
+  @override
+  State<Authentication> createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
